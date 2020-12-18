4f03ff104c444a196fa87c8c711e618353edcd98
VioletViolaVi <vivian.v18723@gmail.com>
2020-12-18 22:44:14 +0000

    Create variables for js portion of app.

--- assets/scritps.js
+++ assets/scritps.js
@@ -1,3 +1,44 @@
 $("document").ready(function () {
-  console.log("hello there again");
+  
+  let flash;
+  let flashOrder = [];
+  let score = 0;
+
+  let green = document.querySelector("#topLeft");
+  let red = document.querySelector("#topRight");
+  let yellow = document.querySelector("#bottomLeft");
+  let blue = document.querySelector("#bottomRight");
+
+  function clickedGreen() {
+    green.addEventListener("click", function () {
+        console.log("hi");
+    });
+  };
+  clickedGreen();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
