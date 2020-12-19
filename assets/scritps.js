3d84833f09012de059ebb9db18674d6976bc01c6
VioletViolaVi <vivian.v18723@gmail.com>
2020-12-19 13:40:30 +0000

    Redo scss style of game.

--- assets/scritps.js
+++ assets/scritps.js
@@ -1,45 +1,45 @@
 $("document").ready(function () {
-  let flash;
-  let flashOrder = [];
+  // let flash;
+  // let flashOrder = [];
 
-  let playerInputOrder = [];
+  // let playerInputOrder = [];
 
-  let score = 0;
-  let highScore = 0;
+  // let score = 0;
+  // let highScore = 0;
 
-  let green = document.querySelector("#topLeft");
-  let red = document.querySelector("#topRight");
-  let yellow = document.querySelector("#bottomLeft");
-  let blue = document.querySelector("#bottomRight");
+  // let green = document.querySelector("#topLeft");
+  // let red = document.querySelector("#topRight");
+  // let yellow = document.querySelector("#bottomLeft");
+  // let blue = document.querySelector("#bottomRight");
 
-  let start = document.querySelector("#middle");
+  // let start = document.querySelector("#middle");
 
-  function startBtn() {
-    start.addEventListener("click", function () {
-      if (start) {
-        console.log("START");
-        beginGame();
-      }
-    });
-  }
-  startBtn();
+  // function startBtn() {
+  //   start.addEventListener("click", function () {
+  //     if (start) {
+  //       console.log("START");
+  //       beginGame();
+  //     }
+  //   });
+  // }
+  // startBtn();
 
-  function beginGame() {
-    let flashOrder = [];
-    let playerInputOrder = [];
-    let score = 0;
-    $("#highScore").hide();
-    $("#restart").hide();
-  }
+  // function beginGame() {
+  //   let flashOrder = [];
+  //   let playerInputOrder = [];
+  //   let score = 0;
+  //   $("#highScore").hide();
+  //   $("#restart").hide();
+  // }
 
-  function greenBtn() {
-    green.addEventListener("click", function () {
-      if (green) {
-        console.log("green");
-      }
-    });
-  }
-  greenBtn();
+  // function greenBtn() {
+  //   green.addEventListener("click", function () {
+  //     if (green) {
+  //       console.log("green");
+  //     }
+  //   });
+  // }
+  // greenBtn();
 
 
 
