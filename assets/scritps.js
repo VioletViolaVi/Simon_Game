d3dc203135914f169a9bcd3901694266770f8809
VioletViolaVi <vivian.v18723@gmail.com>
2020-12-20 21:17:41 +0000

    Make quarters light up for memory part of game.

--- assets/scritps.js
+++ assets/scritps.js
@@ -1,8 +1,17 @@
+// DELETE ALL CONSOLE LOGS AT END
 $("document").ready(function () {
-  
-  let simonFlashOrder = [];
-  let userInputOrder = [];
 
+  let flashLevel = 0;
+
+  let userInputOrder = [];
+  let beepSounds = [
+    "https://freesound.org/people/KorgMS2000B/sounds/54415/",
+    "https://freesound.org/people/altemark/sounds/39747/",
+    "https://freesound.org/people/shukran/sounds/53243/",
+    "https://freesound.org/people/carbilicon/sounds/65243/",
+  ];
+
+  // hides text scores
   function hideScoresText() {
     $("#yourScore").hide();
     $("#highScore").hide();
@@ -10,12 +19,65 @@ $("document").ready(function () {
   }
   hideScoresText();
 
-  $("#startBtn").click(function () {
-    beginGame();
-    console.log("hi");
-  });
+  // starts game round
+  function startRound() {
+    $("#startBtn").click(function () {
+      flashLevel++;
+      handleSimonSequence();
+    });
+  }
+  startRound();
 
-  function beginGame() {}
+  // changes game level
+  function changeGameLevel() {
+    $("#level").text(flashLevel);
+  }
+
+  let indexContent = 0;
+  let flashColour = 0;
+  let simonFlashOrder = [];
+
+  // controls simon sequence
+  function handleSimonSequence() {
+    changeGameLevel();
+    produceRandomNumForSimonFlashOrder();
+    let indexOfSimonArray = 0;
+    let myInterval = setInterval(function () {
+      indexContent = simonFlashOrder[indexOfSimonArray];
+      flashColour = $("#" + indexContent).attr("class").split(" ")[1];
+      console.log("indexContent: " + indexContent + " flashColour: " + flashColour);
+      flashHighlightAndSound(indexContent, flashColour);
+      indexOfSimonArray++;
+      if (indexOfSimonArray === simonFlashOrder.length) {
+        clearInterval(myInterval);
+      }
+    }, 1000);
+  }
+
+  // gets and stores random number for flash order
+  function produceRandomNumForSimonFlashOrder() {
+    let randomNum = Math.floor(Math.random() * 4 + 1);
+    simonFlashOrder.push(randomNum);
+    console.log("simonFlashOrder: " + simonFlashOrder);
+  }
+
+  // added highlight colours and sounds
+  function flashHighlightAndSound(id, flashColour) {
+    $("#" + id).addClass(flashColour +"Active");
+    // playSimonSounds(id);
+    setTimeout(function () {
+      $("#" + id).removeClass(flashColour +"Active");
+    }, 500);
+  }
+
+  // sounds for game
+  function playSimonSounds(id) {}
+
+
+
+
+
+
 
 
 
