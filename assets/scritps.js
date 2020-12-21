f57f0bede9aba30347882787d4fc5fa1b9a5499b
VioletViolaVi <vivian.v18723@gmail.com>
2020-12-21 18:51:40 +0000

    Edit restricted number of levels.

--- assets/scritps.js
+++ assets/scritps.js
@@ -1,44 +1,17 @@
-// DELETE ALL CONSOLE LOGS AT END
 $("document").ready(function () {
-  // green noise
-  function greenSound() {
-    $(".green").click(function () {
-      let greenSound = document.getElementById("beep1");
-      greenSound.play();
-      console.log("greenSound: " + greenSound);
-    });
-  }
-  greenSound();
-
-  // red noise
-  function redSound() {
-    $(".red").click(function () {
-      let redSound = document.getElementById("beep2");
-      redSound.play();
-      console.log("redSound: " + redSound);
-    });
-  }
-  redSound();
-
-  // yellow noise
-  function yellowSound() {
-    $(".yellow").click(function () {
-      let yellowSound = document.getElementById("beep3");
-      yellowSound.play();
-      console.log("yellowSound: " + yellowSound);
-    });
-  }
-  yellowSound();
-
-  // blue noise
-  function blueSound() {
-    $(".blue").click(function () {
-      let blueSound = document.getElementById("beep4");
-      blueSound.play();
-      console.log("blueSound: " + blueSound);
-    });
-  }
-  blueSound();
+  
+  let htmlID = 0;
+  let flashColour = 0;
+  let flashLevel = 0;  
+  let numOfLevels = 500;  
+  let userInputOrder = [];  
+  let simonFlashOrder = [];
+  let beepSounds = [
+    "media/sounds/beep1.mp3",
+    "media/sounds/beep2.mp3",
+    "media/sounds/beep3.mp3",
+    "media/sounds/beep4.mp3",
+  ];
 
   // hides text scores
   function hideScoresText() {
@@ -47,8 +20,6 @@ $("document").ready(function () {
     $("#restart").hide();
   }
   hideScoresText();
-
-  let flashLevel = 0;
 
   // starts game round
   function startRound() {
@@ -59,16 +30,12 @@ $("document").ready(function () {
   }
   startRound();
 
-  let userInputOrder = [];
-
   // for users picking colours
   function userColourInput() {
     $(".quarter").click(function () {
       htmlID = $(this).attr("id");
       flashColour = $(this).attr("class").split(" ")[1];
       userInputOrder.push(htmlID);
-      console.log("htmlID: " + htmlID + " flashColour: " + flashColour);
-      console.log("userInputOrder: " + userInputOrder);
       flashHighlightAndSound(htmlID, flashColour);
       // check user's input order
       if (!compareUserInputWithSimon()) {
@@ -76,10 +43,14 @@ $("document").ready(function () {
         userInputOrder = [];
       }
       // occurs when user's length input is same as simon's
-      if (userInputOrder.length === simonFlashOrder.length) {
+      if (userInputOrder.length === simonFlashOrder.length && userInputOrder.length < numOfLevels) {
         flashLevel++;
         userInputOrder = [];
         handleSimonSequence();
+      }
+      // checking for winners 
+      if (userInputOrder.length === numOfLevels) {
+        $("#level").text("Win");
       }
     });
   }
@@ -98,9 +69,8 @@ $("document").ready(function () {
 
   // shows error
   function displayError() {
-    console.log("ERROR");
     let counter = 0;
-    let myError = setInterval(() => {
+    let myError = setInterval(function() {
       $("#level").text("--");
       counter++;
       if (counter === 3) {
@@ -115,25 +85,18 @@ $("document").ready(function () {
   // changes game level
   function changeGameLevel() {
     $("#level").text(flashLevel);
-    console.log("Level: " + flashLevel);
   }
-
-  let htmlID = 0;
-  let flashColour = 0;
-  let simonFlashOrder = [0, 2, 1];
-  let numOfLevels = 3;
 
   // controls simon sequence
   function handleSimonSequence() {
     changeGameLevel();
-    // produceRandomNumForSimonFlashOrder();
+    produceRandomNumForSimonFlashOrder();
     let indexOfSimonArray = 0;
     let myInterval = setInterval(function () {
       htmlID = simonFlashOrder[indexOfSimonArray];
       flashColour = $("#" + htmlID)
         .attr("class")
         .split(" ")[1];
-      // console.log("htmlID: " + htmlID + " flashColour: " + flashColour);
       flashHighlightAndSound(htmlID, flashColour);
       indexOfSimonArray++;
       if (indexOfSimonArray === simonFlashOrder.length) {
@@ -146,7 +109,6 @@ $("document").ready(function () {
   function produceRandomNumForSimonFlashOrder() {
     let randomNum = Math.floor(Math.random() * 4 + 1);
     simonFlashOrder.push(randomNum);
-    console.log("simonFlashOrder: " + simonFlashOrder);
   }
 
   // added highlight colours and sounds
@@ -157,13 +119,6 @@ $("document").ready(function () {
       $("#" + htmlID).removeClass(flashColour + "Active");
     }, 500);
   }
-
-  let beepSounds = [
-    "media/sounds/beep1.mp3",
-    "media/sounds/beep2.mp3",
-    "media/sounds/beep3.mp3",
-    "media/sounds/beep4.mp3",
-  ];
 
   // sounds for game
   function playSimonSounds(htmlID) {
